3d94320600a5a94b832b2be2b8823b140a93e255
Илья <ilahorosij5822@gmail.com>
2026-04-21 13:12:21 +0300

    Создание моделей и структуры базы данных для интернет-магазина

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Manufacturer, Product, Cart, CartItem
+admin.site.register(Category)
+admin.site.register(Manufacturer)
+admin.site.register(Product)
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'total_price')
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'cart', 'quantity', 'item_total_price')
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название")
+    country = models.CharField(max_length=100, verbose_name="Страна")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название категории")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание категории")
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Название товара")
+    description = models.TextField(verbose_name="Описание")
+
+    photo = models.ImageField(upload_to='products/', verbose_name="Фото товара", blank=True, null=True)
+    
+    price = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        validators=[MinValueValidator(0)], 
+        verbose_name="Цена"
+    )
+    
+    stock = models.IntegerField(
+        validators=[MinValueValidator(0)], 
+        verbose_name="Количество на складе"
+    )
+
+    # Связи (ForeignKey)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, verbose_name="Производитель")
+
+    def __str__(self):
+        return self.name
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return f"Корзина пользователя {self.user.username}"
+
+    @property
+    def total_price(self):
+        # Суммируем стоимость всех элементов в этой корзине
+        return sum(item.item_total_price() for item in self.cart_items.all())
+
+    class Meta:
+        verbose_name = "Корзина"
+        verbose_name_plural = "Корзины"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="cart_items", verbose_name="Корзина")
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, verbose_name="Товар")
+    quantity = models.PositiveIntegerField(default=1, verbose_name="Количество")
+
+    def __str__(self):
+        return f"{self.product.name} ({self.quantity} шт.)"
+
+    def item_total_price(self):
+        return self.product.price * self.quantity
+
+    def clean(self):
+        # Валидация: проверяем наличие на складе
+        if self.quantity > self.product.stock:
+            raise ValidationError(
+                f"Недостаточно товара на складе. Доступно: {self.product.stock}"
+            )
+
+    def save(self, *args, **kwargs):
+        self.full_clean() # Запускаем валидацию перед сохранением
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Элемент корзины"
+        verbose_name_plural = "Элементы корзины"
